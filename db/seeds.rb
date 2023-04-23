1fd3f2604792e31b598d86c99deacbe97ba7b893
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-04-23 10:49:29 +0100

    Added: task model / task controller: index + show actions / views: index + show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+3.times do
+  Task.create(
+    title: Faker::Hobby.activity,
+    details: Faker::Lorem.words(number: 4).join(' ').capitalize,
+    completed: false
+  )
+end
+
+Task.create(
+  title: Faker::Hobby.activity,
+  details: Faker::Lorem.words(number: 4).join(' ').capitalize,
+  completed: true
+)
